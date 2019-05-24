78df5a46e43ea1f9c27089d204429a4616ee4a92
SangHun Lee <ayteneve93@gmail.com>
2019-05-24 15:23:29 +0900

    Regular Commit

--- app/src/main/java/com/gmail/ayteneve93/apex/explorer/activities/entry/EntryActivity.kt
+++ app/src/main/java/com/gmail/ayteneve93/apex/explorer/activities/entry/EntryActivity.kt
@@ -1,13 +1,18 @@
-package com.gmail.ayteneve93.apex.explorer.activities.intro
+package com.gmail.ayteneve93.apex.explorer.activities.entry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gmail.ayteneve93.apex.explorer.R
 
-class IntroActivity : AppCompatActivity() {
+class EntryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        setContentView(R.layout.activity_entry)
+
+
+
+
+
     }
 }
