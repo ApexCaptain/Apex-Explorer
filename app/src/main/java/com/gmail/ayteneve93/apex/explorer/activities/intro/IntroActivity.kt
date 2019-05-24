b47e42c11040cfb2d9cd27021ab42c4663458d2c
SangHun Lee <ayteneve93@gmail.com>
2019-05-24 12:49:02 +0900

    Project Creation

---
+++ app/src/main/java/com/gmail/ayteneve93/apex/explorer/activities/intro/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.gmail.ayteneve93.apex.explorer.activities.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gmail.ayteneve93.apex.explorer.R
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
